f505b51fec0700afd7ab066ba81ad9ad448687b3
kavyats1999 <tskavyasajeev@gmail.com>
2023-07-11 11:26:00 +0530

    digital clock

---
+++ main.js
@@ -0,0 +1,15 @@
+
+
+function clockTime(){
+    let date=new Date()
+
+let hour=date.getHours()
+let minute=date.getMinutes()
+let second=date.getSeconds()
+
+    clock.innerHTML=`${hour}:${minute}:${second}`
+    setTimeout(()=>{clockTime()},1000)
+
+}
+
+clockTime()
